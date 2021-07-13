93285d93791500ab23a96662820e4773826e78aa
harryw123545 <harrywakeling@hotmail.co.uk>
2021-07-13 10:51:40 +0100

    fixed bio sketch link

--- src/bio/bioSketch.js
+++ src/bio/bioSketch.js
@@ -1,37 +1,37 @@
-const sketch = (p) => {
+const bioSketch = (p) => {
 
-    // const canvas = document.getElementById('canvas-container')
+    const canvas = document.getElementById('canvas-container')
   
-    //   p.setup = () => {
-    //     p.createCanvas(canvas.offsetWidth, canvas.offsetHeight)
+      p.setup = () => {
+        p.createCanvas(canvas.offsetWidth, canvas.offsetHeight)
     
-    //     p.angleMode(p.DEGREES);
-    //     p.rectMode(p.CENTER);
-    //     p.background(0);
-    //   }
+        p.angleMode(p.DEGREES);
+        p.rectMode(p.CENTER);
+        p.background(0);
+      }
   
-    //   p.draw = () => {
+      p.draw = () => {
   
-    //     let time = p.frameCount;
-    //     let iter = p.map(p.mouseX, 0, p.width, 0, 3);
-    //     let col = p.map(p.mouseY, 0, p.height, 0.02, 3.5);
+        let time = p.frameCount;
+        let iter = p.map(p.mouseX, 0, p.width, 0, 3);
+        let col = p.map(p.mouseY, 0, p.height, 0.02, 3.5);
         
-    //     p.translate(p.width/2, p.height/2);
+        p.translate(p.width/2, p.height/2);
         
-    //     for(var i = 0; i < 200; i++){
-    //     p.push();
-    //     p.rotate(p.cos(p.frameCount + i * iter) * 80);
-    //     p.stroke(127 + 127 * p.sin(i * col + time), 127 + 127 * p.sin(i * 2 + time), 127 + 127 * p.sin(i * 0.5 + time));
-    //     p.noFill();
-    //     p.rect(0, 0, 620 - i * 3.12, 620 - i * 3.12, 200 - i);
-    //     p.pop();
-    //     }
-    //   }
+        for(var i = 0; i < 200; i++){
+        p.push();
+        p.rotate(p.cos(p.frameCount + i * iter) * 80);
+        p.stroke(127 + 127 * p.sin(i * col + time), 127 + 127 * p.sin(i * 2 + time), 127 + 127 * p.sin(i * 0.5 + time));
+        p.noFill();
+        p.rect(0, 0, 620 - i * 3.12, 620 - i * 3.12, 200 - i);
+        p.pop();
+        }
+      }
   
-    //   p.windowResized = () => {
-    //     p.resizeCanvas(canvas.offsetWidth, canvas.offsetHeight)
-    //     p.background(0);
-    //   }
+      p.windowResized = () => {
+        p.resizeCanvas(canvas.offsetWidth, canvas.offsetHeight)
+        p.background(0);
+      }
   }
   
-  export default sketch
+  export default bioSketch
