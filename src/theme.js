a23bb7e91568c324b1892c8d8388fedbf5e19edd
harryw123545 <harrywakeling@hotmail.co.uk>
2021-07-20 15:35:47 +0100

    webfont attempt 3

--- src/theme.js
+++ src/theme.js
@@ -4,19 +4,14 @@ import Codex2 from './fonts/codex-regular.woff2'
 
 
 const codex = {
-    fontFamily: 'Codex',
-    fontStyle: 'normal',
-    fontDisplay: 'swap',
-    fontWeight: 600,
+    fontFamily: 'codexregular',
+    fontweight: 'normal',
+    fontstyle: 'normal',
     src: `
-      local('Codex'),
-      local('codex-regular'),
       url(${Codex}) format('woff'),
       url(${Codex2}) format('woff2')
-    `,
-    unicodeRange:
-      'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
-  };
+    `
+   };
   
   const theme = createMuiTheme({
     typography: {
