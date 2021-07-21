6aa9cc8947f790831251512a49994314b7b7035c
harryw123545 <harrywakeling@hotmail.co.uk>
2021-07-21 16:04:08 +0100

    updated noise sketch

--- src/type/type.js
+++ src/type/type.js
@@ -14,7 +14,7 @@ const typeSketch = () => {
     let f = 0.0
     let spc
     let numIters
-    let grid = 4
+    let grid = 2
 
     p5.setup = () => {
       let c = p5.createCanvas(canvas.offsetWidth, canvas.offsetHeight)
@@ -24,18 +24,18 @@ const typeSketch = () => {
         c.parent(canvas)
 
         // our grid spacing
-        numIters = grid
+        numIters = 1
         
         // we multiply by 1.0 to avoid a bug in processing,
         // otherwise it counts it as an int
         spc = p5.width * 1. / numIters
 
-
+        p5.background(0)
     }
 
     p5.draw = () => {
   
-      p5.background(0, 10)
+      //p5.background(0, 7)
 
       for (let i = 0; i < numIters; i++) {
         for (let j = 0 ; j < numIters; j++) {
@@ -43,7 +43,8 @@ const typeSketch = () => {
           p5.push()
           
           // move to the current location in the grid
-          p5.translate(spc/2 + i * spc, spc/2 +  j * spc)
+          //p5.translate(spc/2 + i * spc, spc/2 +  j * spc)
+          p5.translate(p5.width/2 + 1, p5.height/2 + 1)
           
           // call a function which draws something at (0, 0)
           p5.drawAMohr( i + 1, j + 1)
@@ -53,18 +54,23 @@ const typeSketch = () => {
       }
       
       // itterate f so we have an animation
-      f += 0.01
+      f += 0.002
     }
 
     p5.drawAMohr = (i, j) => {
 
       let time = p5.frameCount*0.013;
+      let iter = p5.map(p5.mouseX, 0, p5.width, 0.001, 0.1)
     
-      p5.stroke(127 + 127 * p5.sin(i * 0.1 + time), 127 + 127 * p5.sin(i * 0.01 + time), 127 + 127 * p5.sin(i * 0.001 + time))
-      p5.noFill()
+      // p5.stroke(127 + 127 * p5.sin(i * 0.1 + time), 127 + 127 * p5.sin(i * 0.01 + time), 127 + 127 * p5.sin(i * 0.001 + time))
+      
       p5.beginShape()
-      for (let k = 0; k < 15; k++) {
-        p5.vertex(p5.noise( k * j, i + f) * spc  - spc/2, p5.noise( i, j *k + f ) * spc - spc/2)
+      for (let k = 0; k < 26; k++) {
+        //p5.stroke(127 + 127 * p5.sin(k * iter+0.2 + time), 127 + 127 * p5.sin(k * iter+0.1 + time), 127 + 127 * p5.sin(k * iter + time))
+        p5.fill(127 + 127 * p5.sin(k * iter + time), 127 + 127 * p5.sin(k * iter*0.3 + time), 127 + 127 * p5.sin(k * 0.001 + time))
+        p5.noStroke()
+        //p5.noFill()
+        p5.curveVertex(p5.noise( k * j, i + f) * spc  - spc/2, p5.noise( i, j *k + f ) * spc - spc/2)
       }
       p5.endShape()
   }
